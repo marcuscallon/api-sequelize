5b2920f2de6e0ef90613e3c149cc2c381beeae1c
Christoffer Rødvik <christoffer@kodemon.net>
2015-08-08 15:56:01 +0900

    refactored query helpers

---
+++ helpers/query.js
@@ -0,0 +1,81 @@
+'use strict';
+
+let util = require('util');
+
+/**
+ * @class  Query
+ * @param  {Object} options
+ * @param  {Object} schema
+ * @return {Object}
+ */
+let Query = module.exports = function query(options, schema) {
+  let result = { where : {} };
+  let where  = schema.where;
+  for (let key in where) {
+    if (options.hasOwnProperty(key)) {
+      result.where[key] = prepareValue(where[key], options[key]);
+    }
+  }
+  if (options.limit)  { result.limit  = Query.NUMBER(options.limit); }
+  if (options.offset) { result.offset = Query.NUMBER(options.offset); }
+  return result;
+};
+
+/**
+ * @private
+ * @method prepareValue
+ * @param  {Mixed} handler
+ * @param  {Mixed} val
+ * @return {Object}
+ */
+function prepareValue(handler, val) {
+  if (util.isFunction(handler)) {
+    return handler(val);
+  }
+  let result = {};
+  for (let key in handler) {
+    result[key] = prepareValue(handler[key], val);
+  }
+  return result;
+};
+
+/**
+ * @method BOOLEAN
+ * @param  {Mixed} val
+ * @return {Boolean}
+ */
+Query.BOOLEAN = function (val) {
+  switch (val) {
+    case 'true'  : case 1 : return true;
+    case 'false' : case 0 : return false;
+    default : 
+      return val;
+  }
+};
+
+/**
+ * @method STRING
+ * @param  {Mixed} val
+ * @return {String}
+ */
+Query.STRING = function (val) {
+  return String(val);
+};
+
+/**
+ * @method NUMBER
+ * @param  {Mixed} val
+ * @return {Number}
+ */
+Query.NUMBER = function (val) {
+  return Number(val);
+};
+
+/**
+ * @method DATE
+ * @param  {Mixed} val
+ * @return {Date}
+ */
+Query.DATE = function (val) {
+  return Date(val);
+};
