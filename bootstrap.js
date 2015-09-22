236ec162eaf0dd0e960747b06fca4f2a5aea1731
Christoffer Rødvik <christoffer@kodemon.net>
2015-09-22 15:58:13 +0900

    0.1.5 Refactor
     - Bootstrap is now all contained in bootstrap.js
     - Models are no longer loaded from provider, all models are loaded from core.

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,8 @@
 'use strict';
 
-let mysql  = require('mysql');
-let config = Reach.config.sequelize;
+let mysql     = require('mysql');
+let sequelize = Reach.provider('sequelize');
+let config    = Reach.config.sequelize;
 
 module.exports = function *() {
   let host = mysql.createConnection({
@@ -9,6 +10,9 @@ module.exports = function *() {
     user     : config.username,
     password : config.password
   });
+
+  // ### Connection Test
+  // Test to see if the connection is valid.
 
   yield new Promise((resolve, reject) => {
     host.connect((err) => {
@@ -18,6 +22,9 @@ module.exports = function *() {
       resolve();
     });
   });
+
+  // ### Drop Database
+  // Drop database if force is set to true.
 
   if (config.force) {
     yield new Promise((resolve, reject) => {
@@ -30,6 +37,8 @@ module.exports = function *() {
     });
   }
 
+  // ### Create Database
+
   yield new Promise((resolve, reject) => {
     host.query('CREATE DATABASE IF NOT EXISTS ' + config.database + ' CHARACTER SET = utf8 COLLATE = ' + (config.charset || 'utf8_unicode_ci'), (err) => {
       if (err) { 
@@ -38,4 +47,14 @@ module.exports = function *() {
       resolve();
     });
   });
+
+  // ### Sync Models
+
+  yield sequelize.sync({
+    force : config.force
+  });
+
+  // ### Setup Relations
+
+  yield require('./setup');
 };
--- bootstrap/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function *() {
-  yield require('./database');
-  yield require('./models');
-};
